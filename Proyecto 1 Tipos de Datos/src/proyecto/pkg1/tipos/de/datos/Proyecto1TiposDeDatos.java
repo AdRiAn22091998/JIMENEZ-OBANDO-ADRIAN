ba7149a1b5406568fc5b568e23b5b1550507db14
AdRiAn22091998 <jimenezadriano@gmail.com>
2020-03-09 11:05:58 -0500

    Hola
    Comenzamos

---
+++ Proyecto 1 Tipos de Datos/src/proyecto/pkg1/tipos/de/datos/Proyecto1TiposDeDatos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.pkg1.tipos.de.datos;
+
+/**
+ *
+ * @author ITQ
+ */
+public class Proyecto1TiposDeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
