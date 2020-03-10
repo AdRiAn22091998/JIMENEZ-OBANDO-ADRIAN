3362cc449376cec849abb6bcc7c2e218f5b86352
AdRiAn22091998 <jimenezadriano@gmail.com>
2020-03-10 11:07:38 -0500

    Update Proyecto1TiposDeDatos.java
    aumentamos Tipos de Datos con su nombre e igualamos (dimos valores )

--- Proyecto 1 Tipos de Datos/src/proyecto/pkg1/tipos/de/datos/Proyecto1TiposDeDatos.java
+++ Proyecto 1 Tipos de Datos/src/proyecto/pkg1/tipos/de/datos/Proyecto1TiposDeDatos.java
@@ -7,13 +7,25 @@ package proyecto.pkg1.tipos.de.datos;
 
 /**
  *
- * @author ITQ
+ * @author ADRIAN
  */
 public class Proyecto1TiposDeDatos {
 
     /**
      * @param args the command line arguments
      */
+    
+    //Entero -32768 a 32767
+    int numeroEntero = 0;
+    double numeroDouble = 0.0;
+    char letra = 'u';
+    String cadena = "Hola Mundo" ; 
+    // Aqui la primera letra va en mayuscula 
+    boolean verdaderoFalso = false ;
+    //para cambiar el valor solo se llama a la variable (nombre) y se le da otro valor
+    verdaderoFalso = true;
+    boolean verdaderoFalso2 = true ;
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
