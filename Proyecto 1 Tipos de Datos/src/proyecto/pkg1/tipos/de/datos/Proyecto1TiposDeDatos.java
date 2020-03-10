ce24d414ad8e74a7000bb7d6cff10fadc6a6e884
AdRiAn22091998 <jimenezadriano@gmail.com>
2020-03-10 11:55:03 -0500

    Update Proyecto1TiposDeDatos.java
    Suma de dos numeros

--- Proyecto 1 Tipos de Datos/src/proyecto/pkg1/tipos/de/datos/Proyecto1TiposDeDatos.java
+++ Proyecto 1 Tipos de Datos/src/proyecto/pkg1/tipos/de/datos/Proyecto1TiposDeDatos.java
@@ -23,11 +23,31 @@ public class Proyecto1TiposDeDatos {
     // Aqui la primera letra va en mayuscula 
     boolean verdaderoFalso = false ;
     //para cambiar el valor solo se llama a la variable (nombre) y se le da otro valor
-    verdaderoFalso = true;
+   // verdaderoFalso = true;
     boolean verdaderoFalso2 = true ;
     
     public static void main(String[] args) {
         // TODO code application logic here
+imprimirNombre() ;
+suma2Numeros(5,10);
+System.out.println(suma2Numeros(5,10));
+
+// Siempre para poder imprimir colocar System.out.println(suma2Numeros(5,10)<---la funcion);
     }
+    
+    
+    
+      // void esta funcion no es de ningun tipo 
+    
+        public static void imprimirNombre(){
+        System.out.println("Adrian");
+                
+                }
+       public static int suma2Numeros (int num1 ,int num2 ){
+          //declaramos la variable 
+          int suma = num1 + num2 ;
+           return suma ;
+           
+       }
 //Si se puede    
 }
